1121ab773f6bd0ee9c17f9c8e79d9f3690ec1541
Nicolas Delaby <nicolas.delaby@lock8.me>
2015-12-17 14:26:53 +0100

    Make internal state of lazydict resilient to error by preserving its state.

--- test.py
+++ test.py
@@ -23,3 +23,12 @@ class TestLazyDictionary(TestCase):
         d['a'] = 1
         d['b'] = 2
         self.assertEqual(d['sum'], 3)
+
+    def test_atomic_evaluation(self):
+        d = lazydict.LazyDictionary()
+        d['division'] = lambda: 1/0
+        self.assertEqual(d.states['division'], 'defined')
+        self.assertRaises(ZeroDivisionError, d.__getitem__, 'division')
+        # second call checks lazydict.CircularReferenceError is not raised.
+        self.assertRaises(ZeroDivisionError, d.__getitem__, 'division')
+        self.assertEqual(d.states['division'], 'defined')
